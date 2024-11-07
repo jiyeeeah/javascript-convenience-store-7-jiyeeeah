69adf9a7e68cf653a88493223047bfd17c86d1fc
jiyeeeah <sije3757@snu.ac.kr>
2024-11-07 16:06:16 +0900

    test: 환영인사와 재고 출력 성공 테스트 케이스 작성

--- __tests__/ConvenientStoreTest.js
+++ __tests__/ConvenientStoreTest.js
@@ -1,12 +1,52 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import ConvenienceStore from "../src/ConvenienceStore.js";
 
-describe("ConvenienceStore 테스트", async () => {
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("ConvenienceStore 테스트", () => {
   let convenienceStore;
   beforeEach(() => {
     convenienceStore = new ConvenienceStore();
   });
 
-  describe("파일 입출력을 통해 필요한 상품 목록 불러오기", async () => {
+  test("환영인사와 함께 최초의 상품명, 가격, 프로모션 이름, 재고 출력하기", async () => {
+    // given
+    const logSpy = getLogSpy();
     await convenienceStore.init();
+
+    // when
+    convenienceStore.printWelcomeAndInventory();
+
+    const printMessage = [
+      "안녕하세요. W편의점입니다.",
+      "현재 보유하고 있는 상품입니다.",
+      "- 콜라 1,000원 10개 탄산2+1",
+      "- 콜라 1,000원 10개",
+      "- 사이다 1,000원 8개 탄산2+1",
+      "- 사이다 1,000원 7개",
+      "- 오렌지주스 1,800원 9개 MD추천상품",
+      "- 오렌지주스 1,800원 재고 없음",
+      "- 탄산수 1,200원 5개 탄산2+1",
+      "- 탄산수 1,200원 재고 없음",
+      "- 물 500원 10개",
+      "- 비타민워터 1,500원 6개",
+      "- 감자칩 1,500원 5개 반짝할인",
+      "- 감자칩 1,500원 5개",
+      "- 초코바 1,200원 5개 MD추천상품",
+      "- 초코바 1,200원 5개",
+      "- 에너지바 2,000원 5개",
+      "- 정식도시락 6,400원 8개",
+      "- 컵라면 1,700원 1개 MD추천상품",
+      "- 컵라면 1,700원 10개",
+    ];
+
+    // then
+    printMessage.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
   });
 });
