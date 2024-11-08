0189e6927eb7a7e1d37107e1a8fa34c1fd70561d
jiyeeeah <sije3757@snu.ac.kr>
2024-11-08 09:52:43 +0900

    docs: 기능 구현 목록에 예외 상황 추가
    - 상품 수량이 0 이하인 경우 예외 발생

--- src/constant/message.js
+++ src/constant/message.js
@@ -1,6 +1,7 @@
 export const ERROR_MESSAGE = {
-  wrongForm: `[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.`,
-  productNotExist: `[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.`,
-  productOverStock: `[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.`,
-  wrongInput: `[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.`,
+  wrongForm: "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.",
+  productNotExist: "[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.",
+  productOverStock: "[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.",
+  wrongInput: "[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.",
+  productCountNotNegative: "[ERROR] 수량은 1개 이상이어야 합니다. 다시 입력해 주세요.",
 };
