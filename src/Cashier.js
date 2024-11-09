d59d992ab29d510edf3ab8b590c4de02d46f75cd
jiyeeeah <sije3757@snu.ac.kr>
2024-11-09 13:51:11 +0900

    feat: 적용할 프로모션 없는 경우 계산 구현

--- src/Cashier.js
+++ src/Cashier.js
@@ -10,28 +10,30 @@ class Cashier {
   checkout({ productName, productCount, convenienceStore }) {
     const applicablePromotion = convenienceStore.getApplicablePromotion(productName);
     const isPromo = !!applicablePromotion;
-    const buyingProducts = convenienceStore.getProductInfoFromInventory(productName, isPromo);
 
+    // 적용할 프로모션이 없는 경우
     if (!applicablePromotion) {
-      convenienceStore.reduceStockBy(productName, productCount, isPromo);
+      convenienceStore.reduceStockBy(productName, productCount, isPromo); // 재고에서 없애기
+      this.#payment.total += convenienceStore.calculatePrice(productName, productCount); // 계산해야할 total에 저장
       return;
     }
 
-    const promoProduct = buyingProducts.filter(
-      (product) => product.promotion === applicablePromotion.name,
-    )[0];
-    if (promoProduct.quantity < productCount) {
+    if (convenienceStore.isInPromoStock({ productName, productCount })) {
       if (this.#askIfPayWithoutPromo()) {
         // 일부 수량에 대해 정가로 결제
       }
       // 정가로 결제해야 하는 수량 제외하고 결제
     }
-    promoProduct.quantity -= productCount;
+    convenienceStore.reduceStockBy(productName, productCount, isPromo);
   }
 
   #askIfPayWithoutPromo() {
     const answer = InputView.askPromotionStockShortage();
     return answer === "Y";
+  }
+
+  getTotalPurchaseAmount() {
+    return this.#payment.total;
   }
 }
 
