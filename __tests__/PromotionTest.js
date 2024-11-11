00ed1c9e9fb73f449689d15d5bd0ec97b4f2445f
jiyeeeah <sije3757@snu.ac.kr>
2024-11-11 20:49:01 +0900

    test: Promotion 테스트 케이스 추가 및 리팩토링

--- __tests__/PromotionTest.js
+++ __tests__/PromotionTest.js
@@ -6,29 +6,72 @@ describe("프로모션 클래스 테스트", () => {
     promotion = new Promotion();
   });
 
-  test("프로모션 정보를 알맞게 가져온다.", async () => {
-    await promotion.init();
-
-    expect(promotion.getPromotionByName("탄산2+1")).toEqual({
+  const promotionInfoCase = [
+    {
       name: "탄산2+1",
-      buy: 2,
-      get: 1,
-      start_date: "2024-01-01",
-      end_date: "2024-12-31",
-    });
-    expect(promotion.getPromotionByName("MD추천상품")).toEqual({
+      result: {
+        name: "탄산2+1",
+        buy: 2,
+        get: 1,
+        start_date: "2024-01-01",
+        end_date: "2024-12-31",
+      },
+    },
+    {
       name: "MD추천상품",
-      buy: 1,
-      get: 1,
-      start_date: "2024-01-01",
-      end_date: "2024-12-31",
-    });
-    expect(promotion.getPromotionByName("반짝할인")).toEqual({
+      result: {
+        name: "MD추천상품",
+        buy: 1,
+        get: 1,
+        start_date: "2024-01-01",
+        end_date: "2024-12-31",
+      },
+    },
+    {
       name: "반짝할인",
-      buy: 1,
-      get: 1,
-      start_date: "2024-11-01",
-      end_date: "2024-11-30",
-    });
-  });
+      result: {
+        name: "반짝할인",
+        buy: 1,
+        get: 1,
+        start_date: "2024-11-01",
+        end_date: "2024-11-30",
+      },
+    },
+  ];
+
+  test.each(promotionInfoCase)(
+    "$name 프로모션 정보를 알맞게 가져온다.",
+    async ({ name, result }) => {
+      await promotion.init();
+
+      expect(promotion.getPromotionByName(name)).toEqual(result);
+    },
+  );
+
+  const promotionAvailableCase = [
+    {
+      name: "탄산2+1",
+      testDate: "2024-10-10",
+      result: true,
+    },
+    {
+      name: "탄산2+1",
+      testDate: "2025-10-10",
+      result: false,
+    },
+    {
+      name: "반짝할인",
+      testDate: "2024-10-10",
+      result: false,
+    },
+  ];
+  test.each(promotionAvailableCase)(
+    "$name 프로모션이 $testDate에 유효한지 확인한다.",
+    async ({ name, testDate, result }) => {
+      await promotion.init();
+      const date = new Date(testDate);
+
+      expect(promotion.isAvailable(name, date)).toEqual(result);
+    },
+  );
 });
