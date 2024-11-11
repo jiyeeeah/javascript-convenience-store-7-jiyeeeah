e35a8b4bf65cd56f01581eadcebe00e317e3dc1f
jiyeeeah <sije3757@snu.ac.kr>
2024-11-11 22:11:39 +0900

    refactor: 입력 메시지 개행 추가

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -13,7 +13,7 @@ const InputView = {
 
   async askPromotionStockShortage(productName, productCount) {
     const input = await MissionUtils.Console.readLineAsync(
-      `현재 ${productName} ${productCount}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n`,
+      `\n현재 ${productName} ${productCount}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n`,
     );
     if (input !== "Y" && input !== "N") throw new Error(ERROR_MESSAGE.wrongInput);
     return input;
@@ -21,21 +21,23 @@ const InputView = {
 
   async askAddPromotionProduct(productName) {
     const input = await MissionUtils.Console.readLineAsync(
-      `현재 ${productName}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n`,
+      `\n현재 ${productName}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n`,
     );
     if (input !== "Y" && input !== "N") throw new Error(ERROR_MESSAGE.wrongInput);
     return input;
   },
 
   async askMembership() {
-    const input = await MissionUtils.Console.readLineAsync("멤버십 할인을 받으시겠습니까? (Y/N)\n");
+    const input = await MissionUtils.Console.readLineAsync(
+      "\n멤버십 할인을 받으시겠습니까? (Y/N)\n",
+    );
     if (input !== "Y" && input !== "N") throw new Error(ERROR_MESSAGE.wrongInput);
     return input;
   },
 
   async askBuyAgain() {
     const input = await MissionUtils.Console.readLineAsync(
-      "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)\n",
+      "\n감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)\n",
     );
     if (input !== "Y" && input !== "N") throw new Error(ERROR_MESSAGE.wrongInput);
     return input;
